08760a03bfd25f9da3c35b1de4e4be6c6e3e34d7
Luis Alberto Foronda Condori <slushiiforonda@gmail.com>
2023-01-03 11:54:25 -0400

    primer commit

---
+++ public/js/app.js
@@ -0,0 +1,63 @@
+const boton = document.querySelector('#btn-menu');
+const menu = document.querySelector('#menu');
+const slideprev = document.querySelector('#prev');
+const slidenext = document.querySelector('#next');
+
+let slideIndex = 1;
+let contadorSlide = 0;
+
+showSlides(slideIndex);
+
+boton.addEventListener('click', () => {
+    menu.classList.toggle("hidden");
+    boton.classList.toggle("bg-white");
+    boton.classList.toggle("text-gray-900");
+    console.log('hidden');
+});
+
+slideprev.addEventListener('click', ()=>{
+    showSlides(slideIndex -= 1);
+});
+
+slidenext.addEventListener('click', ()=>{
+    showSlides(slideIndex += 1);
+});
+
+
+function showSlides(n) {
+    let i;
+    let slides = [...document.querySelectorAll("#mySlides")];
+    let dots = document.querySelectorAll("#dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.add('hidden');
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" bg-gray-800", "");
+    }
+    slides[slideIndex - 1].classList.remove("hidden");
+    dots[slideIndex - 1].className += " bg-gray-800";
+    contadorSlide = 0;
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function autoSlide() {
+    const actual = Number(document.querySelector('.bg-gray-800').dataset.id);
+    console.log(actual);
+    n = actual + 1;
+    currentSlide(n);
+}
+
+setInterval(contAutoSlide, 1000);
+
+function contAutoSlide(){
+    if(contadorSlide == 4){
+        contadorSlide = 0;
+        autoSlide();
+    }
+    contadorSlide += 1;
+}
