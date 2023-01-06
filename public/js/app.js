cc36406d4a6391864c1ecf47b46a8fccb14debae
LuisCodeForonda <luisalbertoforondac@gmail.com>
2023-01-06 16:02:12 -0400

    Boton de menu corregido

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,7 @@ const menu = document.querySelector('#menu');
 
 boton.addEventListener('click', () => {
     menu.classList.toggle("hidden");
-    boton.classList.toggle("bg-white");
+    boton.classList.toggle("bg-gray-300");
     boton.classList.toggle("text-gray-900");
-    console.log('hidden');
 });
 
